2a3142006c2be40e551eec5c66dfa7d462c61188
Adam Oliveras <theadamoliveras@gmail.com>
2018-10-28 16:18:30 -0700

    Have basic backend boiler plate up and running, server is connected, Mongoose connected, morgan setup

---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const
+    express = require('express'),
+    app = express(),
+    logger = require('morgan'),
+    mongoose =  require('mongoose'),
+    MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/still-here',
+    PORT = process.env.PORT || 3001
+    //usersRoutes = require('./routes/users.js')
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, (err) => {
+    console.log(err || `Connected to MongoDB.`)
+})
+
+app.use(logger('dev'))
+app.use(express.json())
+
+app.get('/api', (req, res) => {
+    res.json({ message: "API root." })
+})
+
+//app.use('/api/users', usersRoutes)
+
+app.listen(PORT, (err) => {
+    console.log(err || `Server running on port ${PORT}.`)
+})
+
