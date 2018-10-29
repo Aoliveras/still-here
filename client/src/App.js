ba19fb9eaab14fddb1fd28c5c6fe1e55f70eb1fa
Adam Oliveras <theadamoliveras@gmail.com>
2018-10-28 21:26:10 -0700

    added CDN for semantic UI, imported BrowserRouter and wrapped app in index.js, cleared boiler plate and now have a basic splash page running, Next step, create components for basic navigation of site and get basic page routing working.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="ui center aligned container">
+        <h1>Still Here.</h1>
+        <h3>An app for those of us who need a little help from our friends.</h3>
       </div>
     );
   }
